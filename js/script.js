93b3c0bd3850faeb646f20b9a7685e28d6c0a1b5
disky <diskyphiter@gmail.com>
2023-07-06 22:51:39 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function setTitle(element) {
+  const text = element.innerText;
+
+  document.title = `${text} | E-Grociers`;
+}
+
+function toggleMenuButton() {
+  document.getElementById("menu-list").classList.toggle("show");
+}
+
+function getStartButton() {
+  const bannerHeight = document.getElementById("hero").clientHeight;
+  const headerHeight =
+    document.getElementsByClassName("header")[0].clientHeight;
+
+  window.scrollBy(0, bannerHeight - headerHeight);
+}
